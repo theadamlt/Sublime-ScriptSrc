113898ce5120ad52bbe98d9d174cc26d453d0a68
theadamlt <lilienfeldt.adam@gmail.com>
2012-06-25 13:06:17 +0200

    Added absolute path to php script to fix bug

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import sublime, sublime_plugin
 import commands
+# import subprocess
+
+path = sublime.packages_path();
 
 
 class scriptsrcCommand(sublime_plugin.WindowCommand, sublime.Window):
+	selected = ''
 
 	def run(self):
 		#script_list = ['jQuery', 'jQuery-UI', 'Chrome Frame', 'swfobject', 'mootools','YUI','ExtJS','prototype','scriptaculous','DOJO']
@@ -10,47 +14,49 @@ class scriptsrcCommand(sublime_plugin.WindowCommand, sublime.Window):
 		self.window.show_quick_panel(script_list, self.callback)
 
 	def callback(self, index):
-		if not self.window.views():
-			self.window.new_file()
+		if (index > -1):
+			# if not self.window.views():
+			# 	self.window.new_file()
 
-		global selected
-		selected =  index
-		type_list = ['Compressed', 'Uncompressed']
-		self.window.show_quick_panel(type_list, self.callback2)
+			self.selected =  index
+			type_list = ['Compressed', 'Uncompressed']
+			self.window.show_quick_panel(type_list, self.callback2)
 
 	def callback2(self, index):
-		if not self.window.views():
-			self.window.new_file()
+		if (index > -1):
 
-		libraries = {
-			0: 'jquery',
-			1: 'jqueryui',
-			2: 'prototype',
-			3: 'mootools',
-			4: 'dojo',
-			5: 'swfobject',
-			6: 'yui',
-			7: 'ext-core',
-			8: 'chrome-frame',
-			9: 'scriptaculous'
-			}
+			if not self.window.views():
+				self.window.new_file()
 
-		lib = libraries[selected]
+			libraries = {
+				0: 'jquery',
+				1: 'jqueryui',
+				2: 'prototype',
+				3: 'mootools',
+				4: 'dojo',
+				5: 'swfobject',
+				6: 'yui',
+				7: 'ext-core',
+				8: 'chrome-frame',
+				9: 'scriptaculous'
+				}
 
-		if index == 0:
-			comp = 'compressed'
-		else:
-			comp = 'uncompressed'
+			lib = libraries[self.selected]
 
-		command = 'php -f scriptsrc.php '+lib+' '+comp
-		output = commands.getoutput(command);
-		if output == 'It seems like you dont have an internet connection':
-			sublime.error_message('Couldn\'t fetch the latest script tag. Please check your internet connection');
-		else:
-			sublime.set_clipboard(output)
-			sublime.status_message('Your script-tag has been added to the clipboard')
+			if index == 0:
+				comp = 'compressed'
+			else:
+				comp = 'uncompressed'
 
-		# view = sublime.Window.active_view()
-		# edit = view.begin_edit()
-  		#view.insert(edit, 0, output)
-  		#view.end_edit(edit)
+			command = 'php -f '+sublime.packages_path()+'/scriptsrc/scriptsrc.php '+lib+' '+comp
+			output = commands.getoutput(command)
+			if output == 'It seems like you dont have an internet connection':
+			 	sublime.error_message('Couldn\'t fetch the latest script tag. Please check your internet connection');
+			else:
+			 	sublime.set_clipboard(output)
+			 	sublime.status_message('Your script-tag has been added to the clipboard')
+
+			# view = sublime.Window.active_view()
+			# edit = view.begin_edit()
+	  		#view.insert(edit, 0, output)
+	  		#view.end_edit(edit)
